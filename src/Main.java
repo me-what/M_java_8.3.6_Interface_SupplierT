4c3a29d5976cb059ae26b3644c45399da6dc3185
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-11-17 17:24:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Supplier<User> userFactory = ()->{
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите имя: ");
+            String name = in.nextLine();
+            return new User(name);
+        };
+
+        User user1 = userFactory.get();
+        User user2 = userFactory.get();
+
+        System.out.println("Имя user1: " + user1.getName());
+        System.out.println("Имя user2: " + user2.getName());
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,10 @@
+class User{
+    private String name;
+    String getName(){
+        return name;
+    }
+
+    User(String n){
+        this.name=n;
+    }
+}
